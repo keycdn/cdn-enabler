3f33d4a4c0568aadcca487ea037820b6a57aadb8
Corey <54749229+coreykn@users.noreply.github.com>
2021-06-15 23:09:37 -0600

    update rewriter regex (#41)
    Update regular expression used to match URLs in the
    `CDN_Enabler_Engine::rewriter()` method. This updates the rewriter to
    also rewrite inline URLs that are in escaped JSON format, for example:
    
    ```
    &quot;url&quot;:&quot;\/wp-content\/uploads\/example.jpg&quot;
    ```
    
    Regex tests:
    
    * Old pattern: https://regex101.com/r/IZiFnF/1
    
    * New pattern: https://regex101.com/r/v0yB7U/1/

--- inc/cdn_enabler_engine.class.php
+++ inc/cdn_enabler_engine.class.php
@@ -218,7 +218,7 @@ final class CDN_Enabler_Engine {
      * rewrite contents
      *
      * @since   0.0.1
-     * @change  2.0.1
+     * @change  2.0.4
      *
      * @param   string  $contents                      contents to parse
      * @return  string  $contents|$rewritten_contents  rewritten contents if applicable, unchanged otherwise
@@ -235,7 +235,7 @@ final class CDN_Enabler_Engine {
 
         $included_file_extensions_regex = quotemeta( implode( '|', explode( PHP_EOL, self::$settings['included_file_extensions'] ) ) );
 
-        $urls_regex = '#(?:(?:[\"\'\s=>,]|url\()\K|^)[^\"\'\s(=>,]+(' . $included_file_extensions_regex . ')(\?[^\/?\\\"\'\s)>,]+)?(?:(?=\/?[?\\\"\'\s)>,])|$)#i';
+        $urls_regex = '#(?:(?:[\"\'\s=>,;]|url\()\K|^)[^\"\'\s(=>,;]+(' . $included_file_extensions_regex . ')(\?[^\/?\\\"\'\s)>,]+)?(?:(?=\/?[?\\\"\'\s)>,&])|$)#i';
 
         $rewritten_contents = apply_filters( 'cdn_enabler_contents_after_rewrite', preg_replace_callback( $urls_regex, 'self::rewrite_url', $contents ) );
 
